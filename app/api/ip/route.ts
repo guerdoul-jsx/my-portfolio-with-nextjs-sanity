7889d844bee5e5006a205404ee6f0c87d349e789
= <=>
2024-11-18 03:57:55 +0100

    updated the build script in pckg 2

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -2,11 +2,32 @@ import { getIPInfo } from "@/actions/ip";
 import prisma from "@/db";
 import { NextResponse } from "next/server";
 
+export async function GET(req: Request, res: Response) {
+  try {
+    const ip_info = await getIPInfo();
+    if (ip_info) {
+      return NextResponse.json(ip_info);
+    } else {
+      return NextResponse.json(
+        { message: "Failed to get IP info" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    console.log("[IPS]_GET_METHOD] error: ", error);
+    return NextResponse.json({
+      message: "Internal Server Error",
+      status: 500,
+      error,
+    });
+  }
+}
+
 export async function POST(req: Request, res: Response) {
   try {
     const { d }: { d: boolean } = await req.json();
     const ip_info = await getIPInfo();
-    if (ip_info) {
+    if (ip_info && d) {
       const ips = await prisma.data.create({
         data: {
           ip: ip_info.ip,
